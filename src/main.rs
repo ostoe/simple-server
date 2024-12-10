89e7d9d3f78ef0f01a1a42553c8718a3da125e6f
linhaifly@163.com <linhaifly@163.com>
2024-12-10 20:57:14 +0800

    add tps.

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,16 @@ use std::sync::Arc;
 use structopt::StructOpt;
 use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::mpsc;
+use chrono::{DateTime, Local, Utc};
+
 // use tokio::time::{sleep, Duration};
 // use tokio::io::{self, AsyncWriteExt};
 
 #[derive(Debug, StructOpt, Clone)]
 #[structopt(name = "tcp-listen", about = "Simple TCP/HTTP Server usage.")]
 struct Opt {
-    /// listen port, 
+    /// listen port,
     // short and long flags (-d, --debug) will be deduced from the field's name
     #[structopt(short = "p", long = "port", default_value = "8080")]
     port: u64,
@@ -27,13 +30,16 @@ struct Opt {
     /// response content path, default "-->OK!"
     #[structopt(short = "r", long, default_value = "")]
     response_path: String,
+
+     /// no log?
+     #[structopt(short = "n", long)]
+     nolog: bool,
 }
 
 #[tokio::main]
 async fn main() {
-    
     let opt = Opt::from_args();
-    let mut rvalue = "-->OK!".to_string();
+    let mut rvalue = "-->OK!\r\n".to_string();
     if opt.response_path != "" {
         let p = std::path::Path::new(&opt.response_path);
         if !p.is_file() {
@@ -43,13 +49,11 @@ async fn main() {
         // let input = File::open(p).unwrap();
         // let buffered = std::io::BufReader::new(input);
         rvalue = fs::read_to_string(opt.response_path).unwrap();
-        
     }
     if opt.level != 4 && opt.level != 7 {
         std::process::exit(1);
     }
-
-
+    let nolog= opt.nolog;
 
     let mut return_value = rvalue;
     let mut notfound = "".to_string();
@@ -65,16 +69,63 @@ async fn main() {
         );
         notfound = format!(
             "{0}{1}{2}{3}{4}",
-            status_fn(404, "NOT FOUND"), server, content_type, format!("Content-Length: {}{}", 0, CRLF), CRLF
+            status_fn(404, "NOT FOUND"),
+            server,
+            content_type,
+            format!("Content-Length: {}{}", 0, CRLF),
+            CRLF
         );
-
     }
     println!("每个请求延时时间:{}ms", opt.sleep);
-	println!("四层or七层？: {}", opt.level);
-	// println!("请求响应内容路径:%s\n", path);
-	println!("监听端口：{}", opt.port);
+    println!("四层or七层？: {}", opt.level);
+    // println!("请求响应内容路径:%s\n", path);
+    println!("监听端口：{}", opt.port);
     println!("响应内容：\n{}", return_value);
-    println!("结束位: {}, ps: 最后一个字符为十进制，如果使用jmeter tcp压测，请设置为", return_value.as_bytes().last().unwrap());
+    println!(
+        "结束位: {}, ps: 最后一个字符为十进制，如果使用jmeter tcp压测，请设置为",
+        return_value.as_bytes().last().unwrap()
+    );
+
+    /*  begin req count server */
+    use tokio::sync::mpsc;
+    let (count_sender, mut count_receiver) = mpsc::unbounded_channel::<u8>();
+    let count_task = tokio::spawn(async move {
+        let mut ticker = tokio::time::interval(tokio::time::Duration::from_millis(1000));
+        let mut count = 0;
+        let mut close_count = 0;
+        // let mut duration = tokio::time::Duration::from_millis(0);
+        loop {
+            tokio::select! {
+                _ = ticker.tick() => {
+                    // let now = tokio::time::Instant::now();
+                    let current_local: DateTime<Local> = Local::now();
+                    let custom_format = current_local.format("%Y-%m-%d %H:%M:%S");
+                    if !nolog {
+                        println!("[{}] req {} r/s, close {:?}", custom_format, count, close_count,);
+                    }
+                    // if count + err_count != 0 {
+                    //     // now("");
+                    // } else {
+                    //     println!("[{:?}] req {:?} r/s, close {:?}", custom_format, count, err_count,);
+                    // }
+                    count = 0;
+                    close_count = 0;
+                }
+                val = count_receiver.recv() => {
+                    match val.unwrap() {
+                        1 => {
+                            count += 1;
+                            // println!("{}", val.unwrap());
+                        }
+                        _ => {
+                            close_count += 1;
+                        }
+                    }
+                    // let d = val.unwrap();
+                }
+            }
+        }
+    });
 
     let return_value_arc = Arc::new(return_value);
     let notfound_arc = Arc::new(notfound);
@@ -84,6 +135,7 @@ async fn main() {
         .unwrap();
     println!("[{}] lisenting...", opt.port);
     loop {
+        let count_sender_clone = count_sender.clone();
         // The second item contains the ip and port of the new connection.
         let (stream, _) = listener.accept().await.unwrap();
         // A new task is spawned for each inbound socket. The socket is
@@ -94,14 +146,23 @@ async fn main() {
         let newnot = Arc::clone(&notfound_arc);
         // 1. 静态、全局变量
         // 2. struct，解决错误 value moved here, in previous iteration of loop
-
-        tokio::spawn(async  move {
-            if let Ok(_) = handle_f(stream, opt_sleep, network_level, &new200, &newnot).await {
+        tokio::spawn(async move {
+            if let Ok(_) = handle_f(
+                stream,
+                opt_sleep,
+                network_level,
+                &new200,
+                &newnot,
+                count_sender_clone,
+            )
+            .await
+            {
                 // println!("----out")
             }
         });
     }
-    
+    count_task.await.unwrap();
+
 }
 
 fn test(n: u64, b: &str) {
@@ -113,13 +174,20 @@ fn status_fn(code: i32, text: &str) -> String {
     format!("HTTP/1.1 {} {}{}", code, text, CRLF)
 }
 
-async fn handle_f(mut stream: TcpStream, opt_sleep: u64, network_level: u8, response: &str, notfound: &str) -> io::Result<()> {
-
+async fn handle_f(
+    mut stream: TcpStream,
+    opt_sleep: u64,
+    network_level: u8,
+    response: &str,
+    notfound: &str,
+    sender: tokio::sync::mpsc::UnboundedSender<u8>,
+) -> io::Result<()> {
     loop {
         let mut buffer = [0; 1500];
         let size = stream.read(&mut buffer).await?;
         if size == 0 {
             // println!("{} 连接已关闭", stream.peer_addr()?);
+            sender.send(0).unwrap();
             return Ok(());
         }
         // println!("{}", std::str::from_utf8( &buffer[..size]).unwrap() );
@@ -128,7 +196,8 @@ async fn handle_f(mut stream: TcpStream, opt_sleep: u64, network_level: u8, resp
         }
         if network_level == 4 {
             stream.write(response.as_bytes()).await?;
-        } else { //  if network_level == 7 
+        } else {
+            //  if network_level == 7
             if buffer.starts_with(b"GET / HTTP/1") {
                 stream.write(response.as_bytes()).await?;
             } else {
@@ -136,8 +205,7 @@ async fn handle_f(mut stream: TcpStream, opt_sleep: u64, network_level: u8, resp
             };
         }
         stream.flush().await?;
-
-
+        sender.send(1).unwrap();
     }
     return Ok(());
 }
